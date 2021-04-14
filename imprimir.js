5327890e09447a174711a964d7bcede8db5eec05
Osvaldo Méndez Monroy <osvaldomm1996@gmail.com>
2021-04-13 22:16:38 -0500

    first class

---
+++ imprimir.js
@@ -0,0 +1,45 @@
+   /* Ejercicio 2 
+     Imprimir Hola Mundo usando codde runner.
+      Se necesito Node Js.
+      Ejemplo: console.log('Hola Mundo') */
+
+
+    // Sirve para imprimir codigo en JavaSrip
+    console.log('Hola Mundo')
+
+    /* Ejercicio 3 
+     Imprimir el tipo de cada una de las variables
+     
+      Ejemplo: 
+      var edad = 24
+
+      console.log('[] es de tipo ====>', typeof edad)*/
+
+
+var edad = 24
+var id = true
+var nombre = 'Osvaldo'
+//var genero = undifined
+var distancia = 1.8
+var contador = null
+var ficha = {
+    nombre: 'Osvaldo',
+    apellido: 'Mendez',
+    edad: 24,
+    location: 'cuajimalpa',
+    cp : 05200,
+    hobbies: ['boxing'],
+}
+
+console.log('[] es de tipo ====>', typeof edad)
+console.log('[] es de tipo ====>', typeof id)
+console.log('[] es de tipo ====>', typeof nombre)
+//console.log('[] es de tipo ====>', typeof genero)
+console.log('[] es de tipo ====>', typeof distancia)
+console.log('[] es de tipo ====>', typeof contador)
+console.log('[] es de tipo ====>', typeof ficha)
+           
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,62 @@
+// ====  Tipos de dato  ====
+// Boolean => true o false
+var myBoolean = true
+// Undefined => undefined
+var myUndefined = undefined
+// String (agrupa un conjunto de caracteres)
+var mario = "delgado"
+// Null (especifica que una variable estará sin valor)
+var myNull = null
+// Number (Especifica un numero, enteros <> flotantes)
+var myIntNumber = 27
+var myFloatNumber = 1.20
+// Objeto (Los objetos contienen una estructura de datos que mantienen un contexto)
+var myObject = {
+  nombre: "Bryan",
+  age: 27,
+  location: undefined,
+  average: 8.90,
+  hobbies: ['Cinema', 'Reading', 'Cycling', {name: "Gaming"}]
+}
+// Arreglos (Son similares a los objetos pero, sus valores van apuntando a un indice)
+var myArray = [1, "Bryan", true, 90.4, null, undefined, {name: "Gaming"}]
+
+
+ /* Ejercicio 1 
+ *  Crear 3 diferentes variables usando cada tipo de dato
+ *  Ejemplo:*/
+  
+  // 3 variables de tipo Boolean 
+   
+   var pago = true
+   var id = true
+   var MayordeEdad = false
+  
+  // 3 variables de tipo number
+   var edad = 24
+   var peso = 74
+   var distancia = 1.8
+  
+  // 3 variables de tipo objeto undifined
+   var undifined = undifined
+  
+  // 3 variables de tipo string
+   var   nombre = 'Osvaldo'
+   var   apellido = 'Mendez'
+   var   location = 'cuajimalpa'
+  
+  // 3 variables de tipo objeto
+   var ficha = {
+       nombre: 'Osvaldo',
+       apellido: 'Mendez',
+       edad: 24,
+       location: 'cuajimalpa',
+       cp : 05200,
+       hobbies: ['boxing'],
+ }
+ 
+    // 3 variables de tipo Null
+ 
+   var contador = null
+   var cuenta = null
+   var lista = null 
